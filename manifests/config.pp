9f48ffac39ea0a3538cff1b0dfa9d88c50ac842b
Paul Otto <paul@ottoops.com>
2015-04-23 16:47:52 -0600

    Fixed scope issues found by puppet-lint

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,55 +12,54 @@
 #
 class marathon::config inherits marathon {
 
-  file { [$marathon_dir, $conf_dir]:
+  file { [$marathon::marathon_dir, $marathon::conf_dir]:
     ensure => directory,
-    owner  => $owner,
-    group  => $group,
+    owner  => $marathon::owner,
+    group  => $marathon::group,
   }
 
   marathon::property {'mesos_role':
-    value   => $mesos_role,
-    dir     => $conf_dir,
+    value   => $marathon::mesos_role,
+    dir     => $marathon::conf_dir,
     file    => 'mesos_role',
-    require => File[$conf_dir],
+    require => File[$marathon::conf_dir],
   }
 
   marathon::property {'event_subscriber':
-    value   => $event_subscriber,
-    dir     => $conf_dir,
+    value   => $marathon::event_subscriber,
+    dir     => $marathon::conf_dir,
     file    => 'event_subscriber',
-    require => File[$conf_dir],
+    require => File[$marathon::conf_dir],
   }
 
   marathon::property {'http_endpoints':
-    value   => $http_endpoints,
-    dir     => $conf_dir,
+    value   => $marathon::http_endpoints,
+    dir     => $marathon::conf_dir,
     file    => 'http_endpoints',
-    require => File[$conf_dir],
+    require => File[$marathon::conf_dir],
   }
 
   marathon::property {'task_launch_timeout':
-    value   => $task_launch_timeout,
-    dir     => $conf_dir,
+    value   => $marathon::task_launch_timeout,
+    dir     => $marathon::conf_dir,
     file    => 'task_launch_timeout',
-    require => File[$conf_dir],
+    require => File[$marathon::conf_dir],
   }
 
   # Create directory just in case Mesos is not installed on system
   # Using mkdir -p to both create the entire path AND avoid managing
   # the directory resource because Mesos module should do so.
-  exec{"create_zk_directory":
-    command => "/bin/mkdir -p ${zk_conf_dir}",
-    creates => "${zk_conf_dir}",
+  exec{'create_zk_directory':
+    command => "/bin/mkdir -p ${marathon::zk_conf_dir}",
+    creates => $marathon::zk_conf_dir,
   }
   ->
   # Marathon relies on this file
-  file { "${zk_conf_dir}/${zk_conf_file}":
+  file { "${marathon::zk_conf_dir}/${marathon::zk_conf_file}":
     ensure  => 'present',
-    content => $zookeeper,
-    owner   => $owner,
-    group   => $group,
-    require => File[$zk_conf_dir],
+    content => $marathon::zookeeper,
+    owner   => $marathon::owner,
+    group   => $marathon::group,
   }
 
 }
